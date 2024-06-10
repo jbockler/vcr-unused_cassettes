2dc890524449e161dca46755df313677685ada9b
Josch Bockler <9265647+jbockler@users.noreply.github.com>
2024-06-10 03:09:37 +0200

    initial commit

---
+++ lib/vcr/unused_cassettes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "unused_cassettes/version"
+
+module Vcr
+  module UnusedCassettes
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/vcr/unused_cassettes_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vcr::UnusedCassettes do
+  it "has a version number" do
+    expect(Vcr::UnusedCassettes::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
