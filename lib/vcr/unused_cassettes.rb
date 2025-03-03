701d33e3d84b8b3e9a1d9186e4fa8df3e897907a
Josch Bockler <9265647+jbockler@users.noreply.github.com>
2025-03-03 17:48:06 +0100

    improve loading of railtie

--- lib/vcr/unused_cassettes.rb
+++ lib/vcr/unused_cassettes.rb
@@ -5,9 +5,10 @@ require "zeitwerk"
 module VCR
   module UnusedCassettes
     @loader = Zeitwerk::Loader.for_gem_extension(VCR)
+    @loader.do_not_eager_load("#{__dir__}/unused_cassettes/railtie.rb")
     @loader.setup
 
-    require_relative "unused_cassettes/railtie" if defined?(Rails)
+    require_relative "unused_cassettes/railtie" if defined?(::Rails)
 
     class Error < StandardError; end
 
--- lib/vcr/unused_cassettes/railtie.rb
+++ lib/vcr/unused_cassettes/railtie.rb
@@ -2,18 +2,12 @@ require "vcr/unused_cassettes"
 
 module VCR
   module UnusedCassettes
-    if defined?(::Rails)
-      class Railtie < Rails::Railtie
-        railtie_name :vcr_unused_cassettes
+    class Railtie < Rails::Railtie
+      railtie_name :vcr_unused_cassettes
 
-        rake_tasks do
-          path = File.expand_path(__dir__)
-          Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
-        end
-      end
-    else
-      # satisfy zeitwerk loading even when rails is not present
-      class Railtie
+      rake_tasks do
+        path = File.expand_path(__dir__)
+        Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
       end
     end
   end
